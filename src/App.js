39aef66902e8b7f94f9a0280aee5b5c21af8a822
allanisjoiceelectraj.sumaya <allanissumaya22@gmail.com>
2024-03-18 14:12:54 -0400

    Added route

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ const App = () => {
                     <Route path="/about" element={<About />} />
                     <Route path="/skills" element={<Skills />} />
                     <Route path="/contact" element={<Contact />} />
-                    <Route path="/" element={<MainContent />} />
+                    {/*<Route path="/" element={<MainContent />} />*/}
                     <Route path="/documentation" element={<DocumentationPage/>} />
                 </Routes>
             </div>
